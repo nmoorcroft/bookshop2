ac9d89df945ace600d37c22ea01123374ebb5c3c
Moorcroft, Neil <Neil.Moorcroft@zuhlke.com>
2016-05-16 18:10:01 +0100

    added /new-book

--- bookshop2-api/src/main/kotlin/com/zuhlke/bookshop2/BooksController.kt
+++ bookshop2-api/src/main/kotlin/com/zuhlke/bookshop2/BooksController.kt
@@ -1,16 +1,16 @@
 package com.zuhlke.bookshop2
 
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.*
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 data class Book(val id: Long, val title: String)
+class NewBook() { var title: String = "" }
 
 @RestController
 class BooksController @Autowired constructor(val jdbcTemplate: JdbcTemplate) {
@@ -24,6 +24,12 @@ class BooksController @Autowired constructor(val jdbcTemplate: JdbcTemplate) {
     fun book(@PathVariable id: Int): ResponseEntity<Book> {
         val results = jdbcTemplate.query("select book_id, title from book where book_id = ?", arrayOf(id), bookMapper)
         return if (results.size == 1) ResponseEntity(results[0], HttpStatus.OK) else ResponseEntity(HttpStatus.NOT_FOUND)
+    }
+
+    @RequestMapping("/new-book")
+    fun newBook(@RequestBody newBook: NewBook): ResponseEntity<String> {
+        jdbcTemplate.update("insert into book (book_id, title) values (null, ?)", newBook.title)
+        return ResponseEntity(HttpStatus.CREATED)
     }
 
 }
