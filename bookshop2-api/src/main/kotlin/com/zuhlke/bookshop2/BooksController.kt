7b5a3b599ac5adc6e7ad8a184bfaca61b2eebb62
Moorcroft, Neil <Neil.Moorcroft@zuhlke.com>
2016-05-16 17:32:11 +0100

    added books/{id}

--- bookshop2-api/src/main/kotlin/com/zuhlke/bookshop2/BooksController.kt
+++ bookshop2-api/src/main/kotlin/com/zuhlke/bookshop2/BooksController.kt
@@ -1,7 +1,12 @@
 package com.zuhlke.bookshop2
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,11 +15,16 @@ data class Book(val id: Long, val title: String)
 @RestController
 class BooksController @Autowired constructor(val jdbcTemplate: JdbcTemplate) {
 
+    val bookMapper: RowMapper<Book> = RowMapper { rs, row -> Book(rs.getLong(1), rs.getString(2)) }
+
     @RequestMapping("/books")
-    fun books(): List<Book> = jdbcTemplate.query(
-        "select book_id, title from book",
-        { rs, rowNum -> Book(rs.getLong("book_id"), rs.getString("title")) }
-    )
+    fun books(): List<Book> = jdbcTemplate.query("select book_id, title from book", bookMapper)
+
+    @RequestMapping("/books/{id}")
+    fun book(@PathVariable id: Int): ResponseEntity<Book> {
+        val results = jdbcTemplate.query("select book_id, title from book where book_id = ?", arrayOf(id), bookMapper)
+        return if (results.size == 1) ResponseEntity(results[0], HttpStatus.OK) else ResponseEntity(HttpStatus.NOT_FOUND)
+    }
 
 }
 
